52d4cca2eaaf0ff5549e7fe380a7c5d5d1fb8d6e
MorrisLin <aka611511@gmail.com>
2024-07-23 19:33:47 +0800

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<script setup>
-import CharacterList from '@/views/CharacterList.vue'
-</script>
+<script setup></script>
 
 <template>
   <div class="flex items-center justify-center flex-col bg-gray-100">
+    <router-link to="/" class="text-4xl font-bold">This is header</router-link>
     <router-view> </router-view>
   </div>
 </template>
